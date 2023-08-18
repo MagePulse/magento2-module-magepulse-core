3f28938fa6bed1431b3142c05b5bde3dc3075e3c
Clive Walkden <clive.walkden@magepulse.com>
2023-08-18 23:51:05 +0100

    Initial Commit

---
+++ Model/ConfigProviderAbstract.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ * MagePulse
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MagePulse Proprietary EULA
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://magepulse.com/legal/magento-license/
+ *
+ * @category    MagePulse
+ * @package     MagePulse_Core
+ * @copyright   Copyright (c) MagePulse (https://magepulse.com)
+ * @license     https://magepulse.com/legal/magento-license/  MagePulse Proprietary EULA
+ *
+ */
+
+declare(strict_types=1);
+
+namespace MagePulse\Core\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Config\ScopeInterface;
+use Magento\Framework\Module\ModuleListInterface;
+
+abstract class ConfigProviderAbstract
+{
+    protected string $pathPrefix = '/';
+
+    protected ScopeConfigInterface $scopeConfig;
+
+    protected ModuleListInterface $moduleList;
+
+    protected array $data = [];
+
+    protected string $moduleCode;
+
+    public function __construct(ScopeConfigInterface $scopeConfig, ModuleListInterface $moduleList)
+    {
+        $this->scopeConfig = $scopeConfig;
+        $this->moduleList = $moduleList;
+        if ($this->pathPrefix === '/') {
+            throw new \LogicException('$pathPrefix should be declared');
+        }
+    }
+
+    /**
+     * Return the config value
+     *
+     * @param string $path
+     * @param int|ScopeInterface|null $storeId
+     * @param string $scopeType
+     * @return string
+     */
+    protected function getValue(string $path, $storeId = null, string $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT): string
+    {
+        // Global store value
+        if ($storeId === null && $scopeType !== ScopeConfigInterface::SCOPE_TYPE_DEFAULT) {
+            return $this->scopeConfig->getValue($this->pathPrefix . $path, $scopeType, $storeId) ?? '';
+        }
+
+        if ($storeId instanceof \Magento\Framework\App\ScopeInterface) {
+            $storeId = $storeId->getId();
+        }
+        $scopeKey = $storeId . $scopeType;
+
+        // Store value retrieve and save to cache if not already set
+        if (!isset($this->data[$path]) || !\array_key_exists($scopeKey, $this->data[$path])) {
+            $this->data[$path][$scopeKey] = $this->scopeConfig->getValue($this->pathPrefix . $path, $scopeType, $storeId) ?? '';
+        }
+
+        // Return from prefilled cache
+        return $this->data[$path][$scopeKey];
+    }
+
+    /**
+     * Check if a value is set
+     *
+     * @param string $path
+     * @param string $scopeType
+     * @param $storeId
+     * @return bool
+     */
+    protected function isSetFlag(string $path, $storeId = null, string $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT): bool
+    {
+        return (bool)$this->getValue($path, $storeId, $scopeType);
+    }
+
+    /**
+     * @return string
+     */
+    public function getExtensionVersion(): string
+    {
+        $moduleInfo = $this->moduleList->getOne($this->moduleCode);
+        return $moduleInfo['setup_version'] ?? '0.0.1';
+    }
+}
---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * MagePulse
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MagePulse Proprietary EULA
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://magepulse.com/legal/magento-license/
+ *
+ * @category    MagePulse
+ * @package     MagePulse_Core
+ * @copyright   Copyright (c) MagePulse (https://magepulse.com)
+ * @license     https://magepulse.com/legal/magento-license/  MagePulse Proprietary EULA
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MagePulse_Core',
+    __DIR__
+);
